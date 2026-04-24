76ff566674055bcbea7f9f5c035becccb1b7a6be
tejaravutla287 <tejaravutla287@gmail.com>
2026-04-24 13:56:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,13 @@ resource "aws_eks_cluster" "poc" {
  
   vpc_config {
     # These two subnets MUST belong to the same VPC
-    subnet_ids = ["subnet-0160ded4c7fafd2fe", "subnet-03cd75537e88e3946"]
+
+    subnet_ids = [
+      "subnet-0d2c14c66f6530651", # us-east-1a
+      "subnet-03dd3ff00040da7ff", # us-east-1b
+      "subnet-04816d32cf91f4879"  # us-east-1c (optional but recommended)
+    ]
+
   }
 }
  
@@ -18,8 +24,13 @@ resource "aws_eks_node_group" "poc_nodes" {
   cluster_name    = aws_eks_cluster.poc.name
   node_group_name = "poc-node-group"
   node_role_arn   = "arn:aws:iam::567017110325:role/eks-node-role-poc"
-  subnet_ids = ["subnet-0160ded4c7fafd2fe", "subnet-03cd75537e88e3946"]
- 
+
+    subnet_ids = [
+      "subnet-0d2c14c66f6530651", # us-east-1a
+      "subnet-03dd3ff00040da7ff", # us-east-1b
+      "subnet-04816d32cf91f4879"  # us-east-1c (optional but recommended)
+    ]
+
   scaling_config {
     desired_size = 2
     max_size     = 3
