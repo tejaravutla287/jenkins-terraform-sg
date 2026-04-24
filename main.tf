9e83b1cfd56a8fec83e7c508d3f98a0b6d4731dc
tejaravutla287 <tejaravutla287@gmail.com>
2026-04-24 12:39:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+# 2. Create the EKS Cluster (The "Brain")
+resource "aws_eks_cluster" "poc" {
+  name     = "poc-cluster"
+  role_arn = "arn:aws:iam::567017110325:role/eks-cluster-role-poc-2"
+ 
+  vpc_config {
+    # These two subnets MUST belong to the same VPC
+    subnet_ids = ["subnet-0160ded4c7fafd2fe", "subnet-024cb83c35bb9158a"]
+  }
+}
+ 
+# 3. Create the Node Group (The "Body" / Servers)
+resource "aws_eks_node_group" "poc_nodes" {
+  cluster_name    = aws_eks_cluster.poc.name
+  node_group_name = "poc-node-group"
+  node_role_arn   = "arn:aws:iam::567017110325:role/eks-node-role-poc"
+  subnet_ids      = ["subnet-0160ded4c7fafd2fe", "subnet-024cb83c35bb9158a"]
+ 
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+ 
+  # High-performance instances for your app
+  instance_types = ["c7i-flex.large"]
+ 
+  # Ensures the cluster is fully active before starting nodes
+  depends_on = [aws_eks_cluster.poc]
+}
